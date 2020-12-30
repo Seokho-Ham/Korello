800f5cc77b78b71151be3be63c85ed55eef65bd1
cocoball02 <seokho9522@gmail.com>
2020-12-30 23:26:24 +0900

    Login Oauth 구조 생성

---
+++ src/components/user/LoginOauth.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const LoginOauth = () => {
+  const [token, setToken] = useState('');
+  const loginButton = () => {};
+  const logoutButton = () => {};
+  return <div></div>;
+};
+
+export default LoginOauth;
