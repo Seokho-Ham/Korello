5ca4475bad1297d25e6430575a35a47fb2f7c8d7
cocoball02 <seokho9522@gmail.com>
2021-02-22 23:22:33 +0900

    recent list 버그 수정

--- src/components/board/board_utils.js
+++ src/components/board/board_utils.js
@@ -5,9 +5,17 @@ const makeRecentList = data => {
   let result = [];
   let lastView = localStorage.getItem('lastView');
   if (lastView !== null && JSON.parse(lastView).length > 0) {
-    let boards = JSON.parse(lastView).map(element => {
-      return data.filter(e => parseInt(e.id) === element)[0];
+    let arr = [];
+    JSON.parse(lastView).forEach(el => {
+      if (!arr.includes(parseInt(el))) {
+        arr.push(parseInt(el));
+      }
     });
+    let boards = arr
+      .map(element => {
+        return data.filter(e => parseInt(e.id) === element)[0];
+      })
+      .filter(el => el);
     result = boards;
   } else {
     result = [];
