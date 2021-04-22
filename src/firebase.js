5aa17fcda9406384982c5cf114e71d1dea62e7c7
cocoball02 <seokho9522@gmail.com>
2021-04-22 13:16:19 +0900

    env 수정

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-// require('dotenv').config();
+require('dotenv').config();
 
 var firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY
-    ? process.env.REACT_APP_APIKEY
-    : process.env.APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN
-    ? process.env.REACT_APP_AUTHDOMAIN
-    : process.env.AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID
-    ? process.env.REACT_APP_PROJECTID
-    : process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKED
-    ? process.env.REACT_APP_STORAGEBUCKED
-    : process.env.STORAGEBUCKED,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID
-    ? process.env.REACT_APP_MESSAGINGSENDERID
-    : process.env.MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID
-    ? process.env.REACT_APP_APPID
-    : process.env.APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID
-    ? process.env.REACT_APP_MEASUREMENTID
-    : process.env.MEASUREMENTID,
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.STORAGEBUCKED,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
 //firebase config 정보로 firebase 시작
