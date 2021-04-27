eb9fa7192e715140b2766c24fe6c5e400523ccde
cocoball02 <seokho9522@gmail.com>
2021-04-27 14:01:48 +0900

    user reducer 구조 수정

--- src/components/card/card_utils.js
+++ src/components/card/card_utils.js
@@ -1,7 +1,7 @@
 import { fetchCard, fetchData, fetchEvents, getRefreshToken } from '../../api';
 import { setCardData } from '../../reducers/card.reducer';
 import { getFields, setFirebaseData } from '../../firebase';
-import { searchUser } from '../../reducers/user.reducer';
+import { userActions } from '../../reducers/user.reducer';
 
 export const getCard = async (uri, dispatch, boardId) => {
   let [cards, code, error] = await fetchCard(uri);
@@ -48,7 +48,7 @@ export const getCard = async (uri, dispatch, boardId) => {
       eventlogs: events,
       memberlist: memberlist,
     };
-    dispatch(searchUser({ userList: userlist }));
+    dispatch(userActions.searchUser({ userList: userlist }));
     dispatch(setCardData(payload));
   } else {
     console.log(error);
--- src/reducers/user.reducer.js
+++ src/reducers/user.reducer.js
@@ -2,27 +2,20 @@ import { createAction, handleActions } from 'redux-actions';
 import setState from './reduxUtils';
 
 //action types
-const LOGIN = '/user/LOGIN';
-const LOGOUT = '/user/LOGOUT';
-const SEARCH = '/user/SEARCH';
-const JOIN = '/user/JOIN';
-const KICK = '/user/KICK';
 
-export const login = createAction(LOGIN);
-export const logout = createAction(LOGOUT);
-export const searchUser = createAction(SEARCH);
-export const joinBoard = createAction(JOIN);
-export const kickBoard = createAction(KICK);
+const SEARCH = '/user/SEARCH';
+
+export const userActions = {
+  searchUser: createAction(SEARCH),
+};
 
 const initialState = {
-  status: false,
+  loginStatus: false,
   userList: [],
 };
 
 const user = handleActions(
   {
-    [LOGIN]: setState,
-    [LOGOUT]: setState,
     [SEARCH]: setState,
   },
   initialState,
